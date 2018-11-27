cff4d88f3d1bcefd1b6a9e84c9d4d07b74db7d32
Peter Gebhard <peter.gebhard@saxsys.de>
2018-11-27 10:16:19 +0100

    changed post actions; used datacontext inside usings

--- WebAssistedSurvey.Service/Controllers/EventsController.cs
+++ WebAssistedSurvey.Service/Controllers/EventsController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using WebAssistedSurvey.Service.Extensions;
 using WebAssistedSurvey.Service.Models;
 
 namespace WebAssistedSurvey.Service.Controllers
@@ -17,11 +13,11 @@ namespace WebAssistedSurvey.Service.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var context = new DataContext();
-
-            var events = context.WebEvents.ToList();
-
-            return new JsonResult(events);
+            using (var context = new DataContext())
+            {
+                var events = context.WebEvents.ToList();
+                return new JsonResult(events);
+            }
         }
 
         // GET api/values/5
@@ -39,83 +35,53 @@ namespace WebAssistedSurvey.Service.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] WebEvent webEvent)
         {
-            JObject obj = JsonConvert.DeserializeObject(value) as JObject;
-            if (obj == null)
+            using (var context = new DataContext())
             {
-                return;
+                context.WebEvents.Add(webEvent);
+                context.SaveChanges();
             }
-
-            var @event = GetEventFromJsonToken(obj);
-
-            var context = new DataContext();
-            context.WebEvents.Add(@event);
-            context.SaveChanges();
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            var context = new DataContext();
-
             var @event = GetWebEventById(id);
             if (@event == null)
             {
                 return;
             }
 
-            foreach (var survey in @event.WebSurveys)
+            using (var context = new DataContext())
             {
-                context.WebSurveys.Remove(survey);
-            }
-
-            context.WebEvents.Remove(@event);
-
-            context.SaveChanges();
-        }
-
-        private WebEvent GetEventFromJsonToken(JObject item)
-        {
-            try
-            {
-                var @event = new WebEvent
+                foreach (var survey in @event.WebSurveys)
                 {
-                    StartDateTime = item.GetValue<DateTime>("StartDateTime"),
-                    IsMultidays = item.GetValue<bool>("IsMultidays"),
-                    EndDateTime = item.GetValue<DateTime>("EndDateTime"),
-                    Title = item.GetValue<string>("Title"),
-                    Summery = item.GetValue<string>("Summery")
-                };
-
-                var eventId = item.GetValue<int>("EventID");
-                if (eventId != 0)
-                {
-                    @event.WebEventID = eventId;
+                    context.WebSurveys.Remove(survey);
                 }
 
-                return @event;
-            }
-            catch
-            {
-                return null;
+                context.WebEvents.Remove(@event);
+
+                context.SaveChanges();
             }
         }
 
         private WebEvent GetWebEventById(int id)
         {
-            var context = new DataContext();
-
-            var @event = context.WebEvents.FirstOrDefault(e => e.WebEventID == id);
-            if (@event == null)
+            using (var context = new DataContext())
             {
-                return null;
+
+                var @event = context.WebEvents.FirstOrDefault(e => e.WebEventID == id);
+                if (@event == null)
+                {
+                    return null;
+                }
+
+                var surveys = context.WebSurveys.Where(s => s.WebEventID == id);
+                @event.WebSurveys = surveys.Any() ? new List<WebSurvey>(surveys) : new List<WebSurvey>();
+
+                return @event;
             }
-
-            var surveys = context.WebSurveys.Where(s => s.WebEventID == id);
-            @event.WebSurveys = surveys.Any() ? new List<WebSurvey>(surveys) : new List<WebSurvey>();
-
-            return @event;
         }
     }
 }
--- WebAssistedSurvey.Service/Controllers/SurveysController.cs
+++ WebAssistedSurvey.Service/Controllers/SurveysController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using WebAssistedSurvey.Service.Extensions;
 using WebAssistedSurvey.Service.Models;
 
 namespace WebAssistedSurvey.Service.Controllers
@@ -15,69 +12,40 @@ namespace WebAssistedSurvey.Service.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            var context = new DataContext();
-
-            var survey = context.WebSurveys.FirstOrDefault(e => e.WebSurveyID == id);
-            if (survey == null)
+            using (var context = new DataContext())
             {
-                return NotFound();
+                var survey = context.WebSurveys.FirstOrDefault(e => e.WebSurveyID == id);
+                if (survey == null)
+                {
+                    return NotFound();
+                }
+
+                return new JsonResult(survey);
             }
-            
-            return new JsonResult(survey);
         }
 
         [HttpGet("forEvent/{id}")]
         public IActionResult ForEvent(int id)
         {
-            var context = new DataContext();
-
-            var surveys = context.WebSurveys.Where(s => s.WebEventID == id);
-            if (!surveys.Any())
+            using (var context = new DataContext())
             {
-                return NotFound();
-            }
+                var surveys = context.WebSurveys.Where(s => s.WebEventID == id);
+                if (!surveys.Any())
+                {
+                    return NotFound();
+                }
 
-            return new JsonResult(surveys);
+                return new JsonResult(surveys);
+            }
         }
 
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] WebSurvey webSurvey)
         {
-            JObject obj = JsonConvert.DeserializeObject(value) as JObject;
-            if (obj == null)
+            using (var context = new DataContext())
             {
-                return;
-            }
-
-            var survey = GetSurveyFromJsonToken(obj);
-
-            var context = new DataContext();
-            context.WebSurveys.Add(survey);
-            context.SaveChanges();
-        }
-
-        private WebSurvey GetSurveyFromJsonToken(JObject item)
-        {
-            try
-            {
-                var webSurvey = new WebSurvey
-                {
-                    Created = item.GetValue<DateTime>("Created"),
-                    BadGuy = item.GetValue<string>("BadGuy"),
-                    GoodGuy = item.GetValue<string>("GoodGuy"),
-                    ContactName = item.GetValue<string>("ContactName"),
-                    ContactEmail = item.GetValue<string>("ContactEmail"),
-                    Feedback = item.GetValue<string>("Feedback"),
-                    Source = item.GetValue<string>("Source"),
-                    WantNewsletter = item.GetValue<bool>("WantNewsletter"),
-                    WebEventID = item.GetValue<int>("EventID")
-                };
-                
-                return webSurvey;
-            }
-            catch
-            {
-                return null;
+                context.WebSurveys.Add(webSurvey);
+                context.SaveChanges();
             }
         }
     }
